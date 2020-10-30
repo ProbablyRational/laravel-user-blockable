0a29bd2cde02ddd13b40d5bc37ced8b1d038c7bc
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2020-10-30 17:44:24 +0000

    Update BlockRelationship.php

--- src/Models/BlockRelationship.php
+++ src/Models/BlockRelationship.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Hareku\LaravelBlockable\Models;
+namespace ProbablyRational\LaravelBlockable\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
